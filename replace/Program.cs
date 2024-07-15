0f99778aa8fc110ecd7191b7f68f238bd743b4dd
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-15 17:33:28 +0300

    Add project files.

---
+++ replace/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information;
+int n = int.Parse(Console.ReadLine());
+int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+for (int i = 0; i < n; i++)
+{
+    if (arr[i] > 0)
+    {
+        arr[i] = 1;
+    }
+    else if (arr[i] < 0)
+    {
+        arr[i] = 2;
+    }
+    else if(arr[i] == 0) 
+    {
+        arr[i] = 0;
+    }
+
+}
+foreach (int x in arr)
+{
+    Console.Write(x);
+    Console.Write(' ');
+}
+Console.ReadLine();
+
